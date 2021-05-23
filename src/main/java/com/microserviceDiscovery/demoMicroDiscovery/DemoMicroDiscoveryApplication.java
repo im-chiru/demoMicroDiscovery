2a6597ce12ccfc14f7eaffc4c7bfc522fe6047c2
hemanthpatel1999 <hemanthpatel1999@gmail.com>
2021-05-23 21:12:52 +0530

    Initial commit

---
+++ src/main/java/com/microserviceDiscovery/demoMicroDiscovery/DemoMicroDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.microserviceDiscovery.demoMicroDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microserviceDiscovery/demoMicroDiscovery/DemoMicroDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserviceDiscovery.demoMicroDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
